25046af7d6fcc26dab3b78c971de23cb1dec7f92
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-24 15:43:02 +0600

    Update SetCombinationCreator.java

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -4,6 +4,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SetCombinationCreator {
-    public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+   public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> result = new HashSet<>();
+        for (String element : firstSet) {
+            if (secondSet.contains(element) && !thirdSet.contains(element)) {
+                result.add(element);
+            }
+        }
+        for (String element : thirdSet) {
+            if (!firstSet.contains(element) && !secondSet.contains(element)) {
+                result.add(element);
+            }
+        }
+
+        return result;
     }
 }
